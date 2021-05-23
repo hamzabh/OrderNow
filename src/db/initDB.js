d43860a5284a3d78d755b2f615fe37990accf1ca
hamzabh <boudouh.h@gmail.com>
2021-05-23 21:37:23 +0200

    initDB create table user if not exist

---
+++ src/db/initDB.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { KNEX } = require('../db/connexion');
+
+async function initDB() {
+	const connexion = await KNEX
+	if (!await connexion.schema.hasTable('user')) {
+		console.log('initDB');
+		await connexion.schema.createTable('user', table => {
+			table.increments('id');
+			table.string('name');
+			table.string('email');
+			table.unique('id');
+		});
+	} else {
+		console.log('Table user already exist')
+	}
+}
+
+module.exports = {
+	initDB
+};
